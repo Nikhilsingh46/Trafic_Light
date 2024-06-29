328b7bda46edf8c8d64b18af53477217bc901ffb
Nikhil kumar <143347074+Nikhilsingh46@users.noreply.github.com>
2024-06-29 19:31:02 +0530

    Add files via upload

---
+++ APP.js
@@ -0,0 +1,16 @@
+let color = "red";
+
+switch (color) {
+  case "red":
+    console.log("Stop: Trafic Light is Red.");
+    break;
+  case "yellow":
+    console.log("Slow down: Trafic Light is Yellow.");
+    break;
+  case "green":
+    console.log("Go: Trafic Light is Green.");
+    break;
+  default:
+    console.log("Trafic Light is Broken.");
+    break;
+}
